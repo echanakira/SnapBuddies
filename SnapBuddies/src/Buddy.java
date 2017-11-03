9d1adbcdcfbe4a05188f1f40af5625b2f94dc165
Elijah Chanakira <eli2233@msn.com>
2017-11-03 02:02:39 -0400

    Initial Commit

---
+++ SnapBuddies/src/Buddy.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Buddy {
+	
+	private String name;
+	private ArrayList<Buddy> prevBuds;
+	private ArrayList<Buddy> currBuds;
+	private ArrayList<Buddy> totalPrev;
+	private ArrayList<Buddy> buddiesLeft;
+
+	/**
+	 *  A buddy is an individual
+	 *  @param name
+	 *  @param prevBuds			Buddies from previous session
+	 *  @param currBuds			Current list of buddies
+	 *  @param totalPrev		List of total past buddies
+	 *  @param budsLeft 		List of total buddies remaining
+	 */
+	public Buddy(String name, ArrayList<Buddy> previousBuds, 
+			ArrayList<Buddy> currBuds, ArrayList<Buddy> totalPrevious, 
+			ArrayList<Buddy> buddiesLeft) {
+		name = this.name;
+		previousBuds = this.prevBuds;
+		currBuds = this.currBuds;
+		totalPrevious = this.totalPrev;
+		buddiesLeft = this.buddiesLeft;
+	}
+	
+	/* Adds new buddy to the list of remaining buddies */
+	public boolean addBuddy(Buddy bud) {
+		if( buddiesLeft.contains(bud)) {
+			return false;
+		}else {
+			return buddiesLeft.add(bud);
+		}
+	}
+	
+	
+	public static void main(String[] args) throws IOException  {
+		Scanner scan = new Scanner(new File("C:\\Users\\Elijah\\eclipse-workspace\\SnapBuddies\\src\\buddies.txt"));
+		System.setOut(new PrintStream("C:\\Users\\Elijah\\eclipse-workspace\\SnapBuddies\\src\\output.txt"));
+		String line = scan.nextLine();
+		System.out.println(line);
+	}
+}
